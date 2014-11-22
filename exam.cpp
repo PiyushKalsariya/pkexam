49d54532f9893dbddf7ae03a82cf5728014dda57
piyushkalsariya <piyushkalasariya555@gmail.com>
2014-11-22 09:27:23 +0530

    Exam is declared

---
+++ exam.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+
+void main()
+{
+//Declaration part(function 1)
+int CA1,CA2,TermPaper
+cout<<"Enter the value of CA 1";
+cin>>CA1
+cout<<"Enter the value of CA2";
+cin>>CA2
+cout<<"Enter the value of Termpaper";
+cin>>TermPaper
+
+
+
+}
