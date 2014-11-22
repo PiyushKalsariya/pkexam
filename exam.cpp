d467d3922369804684ccc59b7e977d2e3febc7d9
piyushkalsariya <piyushkalasariya555@gmail.com>
2014-11-22 09:35:26 +0530

    Function 2 find Best two part2 added

--- exam.cpp
+++ exam.cpp
@@ -5,13 +5,38 @@
 void main()
 {
 //Declaration part(function 1)
-int CA1,CA2,TermPaper
+int CA1,CA2,TermPaper,Best1,Best2;
 cout<<"Enter the value of CA 1";
 cin>>CA1
 cout<<"Enter the value of CA2";
 cin>>CA2
 cout<<"Enter the value of Termpaper";
 cin>>TermPaper
+
+//Find the best two (function 2)
+if(CA1>CA2)
+{
+   if(CA1>CA3)
+	{
+		 Best1 = CA1;
+        }
+   else
+	{
+		Best2 = CA2;
+        }
+}
+else
+{
+if(CA2>CA3)
+ {
+   Best1 = CA1;
+ }
+else
+ {
+  Best2 = CA2;
+ }
+}
+
 
 
 
